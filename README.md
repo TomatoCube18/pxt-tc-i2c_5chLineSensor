378040fd5331825a196e3190a90a517209470f6c
Tomato Cube <47109215+TomatoCube18@users.noreply.github.com>
2023-06-04 13:47:16 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# pxt-tc-i2c_5chLineSensor
+# MakeCode Package for I2C TomatoCube 5 Channel LineSensor.
+
+This library provides a Microsoft Makecode package for the  TomatoCube 5 Channel LineSensor with I2C Interface.
+Make sure the sensitivy is properly calibrated for your application. 
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
---
+++ i2c_5chLineSensor.ts
@@ -0,0 +1,41 @@
+/**
+  * TC-MAKECODE-I2C-5CH_LineSensor
+  */
+  //% color="#275C6B" icon="\uf1ca weight=95 block="I2C-LineSensor"
+namespace i2cLineSensor {
+    let LINESENSOR_I2C_ADDR = 0x17 
+    
+    
+    /**
+     * Read IRs from 5 Channel Line Sensor.
+     */
+    //% blockId="linesensor_read_ir" block="i2c,IR(Binary)"
+    //% weight=100 
+    export function readIR(): number {
+        
+        // pins.i2cWriteNumber(
+        //     LINESENSOR_I2C_ADDR,
+        //     1,
+        //     NumberFormat.UInt8BE,
+        //     false
+        // )
+        // basic.pause(200)
+        
+        let readbuf = pins.i2cReadBuffer(LINESENSOR_I2C_ADDR, pins.sizeOf(NumberFormat.UInt8LE))
+        let irReadings = readbuf & 0x1F
+
+        return irReadings       
+    }
+
+    /**
+     * Query IR from 5 Channel Line Sensor at position x.
+     */
+    //% blockId="is_line_detected"
+    //% block="Is Line detected at %pos"
+    //% pos.min=1 group.max=5
+    //% weight=90 
+    export function isLineDetected(pos: number): number {
+        let readbuf = pins.i2cReadBuffer(LINESENSOR_I2C_ADDR, pins.sizeOf(NumberFormat.UInt8LE))
+        return ((readbuf & (0x1 << (number-1))) != 0 )
+    }
+}
